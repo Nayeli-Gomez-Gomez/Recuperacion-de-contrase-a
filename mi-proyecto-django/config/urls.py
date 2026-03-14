feac7e263456594bfdc2b8813e8ca8ae6c067341
Nayeli-Gomez-Gomez <L22270468@TUXTLA.TECNM.MX>
2026-03-13 23:25:11 -0600

    Add files via upload

---
+++ mi-proyecto-django/config/urls.py
@@ -0,0 +1,60 @@
+# config/urls.py
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from users.views import index, inicio, register
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', index, name='index'),
+    path('inicio/', inicio, name='inicio'),
+
+    # LOGIN y LOGOUT (del tutorial anterior)
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # REGISTRO (nuevo)
+    path('register/', register, name='register'),
+
+
+    # ... tus otras urls ...
+
+    # Paso 1: Formulario para ingresar email
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='registration/password_reset.html'
+        ),
+        name='password_reset'
+    ),
+
+    # Paso 2: Confirmación de que el email fue enviado
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+
+    # Paso 3: Formulario para nueva contraseña (enlace del email)
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+
+    # Paso 4: Confirmación final
+    path(
+        'password-reset-complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+]
+
+
